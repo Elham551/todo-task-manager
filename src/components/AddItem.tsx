66782bf67100f7554a0dba109f1096cb32294e3e
Elham Souri <ElhamSouri@outlook.com>
2025-04-07 13:41:05 +0200

    Red border while adding empty text

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -6,11 +6,15 @@ type AddItemProperties = {
 export const AddItem = (items: AddItemProperties) => {
 
     const [text, setText] = useState("")
+    const [error, setError] = useState(false);
 
     const handleClick = () => {
-        if (text !== "") {
-            items.onAdd(text)
-            setText("")
+        if (text.trim()) {
+            items.onAdd(text);
+            setText("");
+            setError(false);
+        } else {
+            setError(true);
         }
     }
     return (<>
@@ -18,9 +22,13 @@ export const AddItem = (items: AddItemProperties) => {
             <input
                 type="text"
                 value={text}
-                onChange={(e) => setText(e.target.value)}
+                onChange={(e) => {
+                    setText(e.target.value);
+                    setError(false);
+                }}
                 placeholder="Add new todo..."
-                className="flex-1 border-2 border-gray-200 p-2 rounded focus:outline-none focus:border-indigo-500"
+                className={`flex-1 border-2 p-2 rounded focus:outline-none ${error ? "border-red-500" : "border-gray-200 focus:border-indigo-500"
+                    }`}
             />
             <button
                 onClick={handleClick}
